29d6031bf56ed4b935e625b7268c4ffe7cbb78b4
nakashima <email>
2024-04-18 11:42:22 +0900

    うらない以外の大枠は完成してるはず

--- BlazorGroupB/Models/ThreadMessageClass.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace BlazorGroupB.Models;
-
-public class ThreadMessageClass
-{
-    public Messages? messages { get; set; } = new Messages();
-    public Threads? threads { get; set; } = new Threads();
-}
