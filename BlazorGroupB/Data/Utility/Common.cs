b8acc178b3e40b2e33d5619c565531ca6a7eabed
nakashima <email>
2024-04-23 13:13:23 +0900

    common追加後の細部編集

--- BlazorGroupB/Data/Utility/Common.cs
+++ BlazorGroupB/Data/Utility/Common.cs
@@ -13,9 +13,12 @@ public class Common
     }
 
     /// <summary>
-    /// メッセージを作成する
+    /// Daoを使ってメッセージを作成する
     /// </summary>
+    /// <param name="msg"></param>
     /// <param name="number"></param>
+    /// <param name="userdataID"></param>
+    /// <exception cref="Exception"></exception>
     public void PostMessage(Messages msg, int number, string userdataID)
     {
         try
@@ -45,6 +48,13 @@ public class Common
 
     }
 
+    /// <summary>
+    /// Daoを使ってスレッドを作成する
+    /// </summary>
+    /// <param name="th"></param>
+    /// <param name="userdataID"></param>
+    /// <returns></returns>
+    /// <exception cref="Exception"></exception>
     public int NewThread(Threads th, string userdataID)
     {
         try
@@ -68,14 +78,28 @@ public class Common
 
     }
 
+    /// <summary>
+    /// Daoを使ってUserを作成する
+    /// </summary>
+    /// <param name="userAgent"></param>
+    /// <returns></returns>
+    /// <exception cref="Exception"></exception>
     public string CreateUserID(string userAgent)
     {
-        Users user = new Users();
-        user.UserAgent = userAgent;
-        user.UserCreateDate = DateTime.Now;
+        try
+        {
+            Users user = new Users();
+            user.UserAgent = userAgent;
+            user.UserCreateDate = DateTime.Now;
 
-        //  Userを作成する
-        return connectDao.usersDao.Insert(user);
+            //  Userを作成する
+            return connectDao.usersDao.Insert(user);
+        }
+        catch
+        {
+            throw new Exception("CreateUserIDエラー");
+        }
+
     }
 
 
